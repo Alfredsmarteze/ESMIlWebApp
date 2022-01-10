299ef7e3ec441b29cdc3d8a1030e44b24cf9c597
Alfred Smart <alfredsmarteze@outlook.com>
2022-01-10 15:52:00 +0100

    Setting up project.

---
+++ ESMIlWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ESMIlWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
